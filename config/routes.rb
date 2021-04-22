d34c5e2b91e778ccb94295632eba4d6eb033619c
nejdetkadir <nejdetkadir.550@gmail.com>
2021-04-22 21:19:56 +0300

    Create profile page with by username

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # general
+  get ':id', to: 'pages#profile', as: :profile_page
 end
