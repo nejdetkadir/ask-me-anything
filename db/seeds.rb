36112e19b072373ac4862b7f7642d906ed90553e
nejdetkadir <nejdetkadir.550@gmail.com>
2021-04-22 20:59:09 +0300

    Add faker gem and create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |user|
+  User.create(
+    email: Faker::Internet.email,
+    password: "123456789",
+    password_confirmation: "123456789",
+    fullname: Faker::Name.name_with_middle,
+    username: Faker::Name.first_name.downcase,
+    bio: Faker::Lorem.sentence,
+    location: Faker::Address.state,
+    website: Faker::Internet.url,
+    profile_img: Faker::Avatar.image, 
+    background_img: "https://images.unsplash.com/photo-1619090248278-52a392aae286?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    birthday: Faker::Date.between(from: 20.years.ago, to: Date.today))
+end
