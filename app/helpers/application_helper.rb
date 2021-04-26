da6befb99038820a30f6bd51418552a4fce40705
nejdetkadir <nejdetkadir.550@gmail.com>
2021-04-26 22:58:32 +0300

    Add badge to header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def liked_it(user_id, ask_id)
     Like.where(ask_id: ask_id, user_id: user_id).first
   end
+
+  def asks_count
+    Ask.where(friend_id: current_user.id, answer: "", answer_image: nil).count
+  end
 end
