e7c1f4ffa23a40a7f144edfe53493ac8f6483552
nejdetkadir <nejdetkadir.550@gmail.com>
2021-04-26 23:02:27 +0300

    Add badge of requested_friends count to header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,8 @@ module ApplicationHelper
   def asks_count
     Ask.where(friend_id: current_user.id, answer: "", answer_image: nil).count
   end
+
+  def friend_request_count
+    current_user.requested_friends.count
+  end
 end
